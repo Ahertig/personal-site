ec1111e223048adb32a31bbd6c26d54b33eb67f5
Ahertig <alyssa.hertig@gmail.com>
2016-04-13 19:17:47 -0400

    blog links are now clickable

--- browser/js/blog/blog.js
+++ browser/js/blog/blog.js
@@ -1,45 +1,37 @@
 app.config(function ($stateProvider) {
 
-    // Register our *about* state.
+    // Register the blog state.
     $stateProvider.state('blog', {
-        url: '/blog',
-        controller: 'BlogController',
-        templateUrl: 'js/blog/blog.html',
-        resolve: {
-          allPosts: function(BlogFactory) {
-            console.log('what')
-            return BlogFactory.fetchPosts();
-          }
+      url: '/blog',
+      controller: 'BlogController',
+      templateUrl: 'js/blog/blog.html',
+      resolve: {
+        allPosts: function(BlogFactory) {
+          return BlogFactory.fetchPosts();
         }
+      }
+    })
+    .state('post', {
+      url: '/blog/:id',
+      templateUrl: 'js/blog/singlePost.html',
+      controller: 'PostController',
+      resolve: {
+        thePost: function(BlogFactory, $stateParams){
+          return BlogFactory.fetchPost($stateParams.id)
+        }
+      }
     });
-    // .state( , {
-    //   url: '/blog/:id',
-    //   templateUrl: 'js/blog/singlePost.html'
-    // })
 
 });
 
 app.controller('BlogController', function ($scope, allPosts) {
 
-  console.log('all posts', allPosts)
-
   $scope.posts = allPosts;
 
 });
 
-app.factory('BlogFactory', function ($http) {
-  var BlogFactory = {};
+app.controller('PostController', function ($scope, thePost) {
 
-  BlogFactory.fetchPosts = function() {
-    return $http.get('/api/posts')
-    .then(function(response) {
-      console.log('here is the response data', response.data)
-      return response.data;
-    })
-  }
-
-  return BlogFactory;
+  $scope.post = thePost;
 
 });
-
-
