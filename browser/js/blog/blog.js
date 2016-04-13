041ba3dc7bd6e6fe6285e424088334cb89ae34fd
Ahertig <alyssa.hertig@gmail.com>
2016-04-13 18:22:54 -0400

    blog posts are appearing in the blog state

--- browser/js/blog/blog.js
+++ browser/js/blog/blog.js
@@ -4,12 +4,42 @@ app.config(function ($stateProvider) {
     $stateProvider.state('blog', {
         url: '/blog',
         controller: 'BlogController',
-        templateUrl: 'js/blog/blog.html'
+        templateUrl: 'js/blog/blog.html',
+        resolve: {
+          allPosts: function(BlogFactory) {
+            console.log('what')
+            return BlogFactory.fetchPosts();
+          }
+        }
     });
+    // .state( , {
+    //   url: '/blog/:id',
+    //   templateUrl: 'js/blog/singlePost.html'
+    // })
 
 });
 
 app.controller('BlogController', function ($scope, allPosts) {
 
+  console.log('all posts', allPosts)
+
+  $scope.posts = allPosts;
 
 });
+
+app.factory('BlogFactory', function ($http) {
+  var BlogFactory = {};
+
+  BlogFactory.fetchPosts = function() {
+    return $http.get('/api/posts')
+    .then(function(response) {
+      console.log('here is the response data', response.data)
+      return response.data;
+    })
+  }
+
+  return BlogFactory;
+
+});
+
+
