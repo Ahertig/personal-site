fa595483960a5889fff3b2a5b2214c37bf863f7d
Ahertig <alyssa.hertig@gmail.com>
2016-04-28 12:23:50 -0400

    changes

--- server/app/routes/posts/index.js
+++ server/app/routes/posts/index.js
@@ -5,26 +5,46 @@ var _ = require('lodash');
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post')
 var jwt = require('jsonwebtoken');
+var User = mongoose.model('User');
 
 
 router.post('/login', function(req, res) {
   var payload = req.body;
 
-  var token = jwt.sign(payload, 'shhhhh');
+  if (payload.email) {
+    User.find({email: payload.email})
+    .then(function(user) {
+      console.log('wait this is da user', user)
+      console.log('here are the passwords ', user[0].password, payload.password);
+      if (user.password === payload.password) {
+        var response = {
+          data: {
+            token: jwt.sign(payload, 'shhhhh')
+          }
+        }
+        console.log('and here is the response', response)
+        res.json(response);
+      } else {
+        // rejection message
+      }
+    })
+  } else if (payload.phone) {
+
+  }
 });
 
 router.get('/token-refresh', function(req, res) {
-  Post.find()
-  .then(function(posts) {
-    res.json(posts);
-  })
+  // Post.find()
+  // .then(function(posts) {
+  //   res.json(posts);
+  // })
 });
 
 router.get('/invalidate-token', function(req, res) {
-  Post.find()
-  .then(function(posts) {
-    res.json(posts);
-  })
+  // Post.find()
+  // .then(function(posts) {
+  //   res.json(posts);
+  // })
 });
 
 
